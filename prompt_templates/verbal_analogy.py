c2a1d8452e4d6203bf975109ec481f914451c654
srishti <srishtiyadav0807.com>
2024-05-08 00:22:17 +0200

    update with zero shot CoT prompt

--- prompt_templates/verbal_analogy.py
+++ prompt_templates/verbal_analogy.py
@@ -11,4 +11,10 @@ def create_prompt(self, p, correct=True):
     """
     prompt = self.context + '\n\n' if self.context and p != 0 else ""
     prompt += f"{self.A[self.prob_order[p]]} : {self.B[self.prob_order[p]]} :: {self.C[self.prob_order[p]]} : "
-    return prompt + (self.D[self.prob_order[p]] if correct else self.D_prime[self.prob_order[p]])
+
+    # TO DO: Add CoT prompt here
+    zero_shot = " Let's think step by step"
+    promptwithCot = prompt + zero_shot
+
+    # self.D[self.prob_order[p] -- Chekc what is this?
+    return promptwithCot + (self.D[self.prob_order[p]] if correct else self.D_prime[self.prob_order[p]])
