f7a3e774ff66bc4c251ed7ca8d73e173d953713c
srishti <srishtiyadav0807.com>
2024-05-20 00:11:14 +0200

    wip-eval-files

---
+++ eval/eval_all_verbal_analogies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the list of models
+# models=("llama3" "mistral7b" "starling7b-beta")  # Add your models to this list
+models=("mistral7b")  # Add your models to this list
+
+
+# Define the directory to search for text files
+directory="prompt_templates/story_analogies"  # Change this to your target directory
+
+# Iterate over each model
+for model in "${models[@]}"; do 
+    echo "Starting model:"
+    echo $model
+
+    # Find all text files in the directory and its subdirectories
+    find "$directory" -type f -name '*.txt' | while read -r file; do
+        # Run the command for each text file with the current model
+        echo $file
+        python3 eval/eval_verbal_analogy_new.py --model "$model" --prompt "$file"
+    done
+done
