3f00d0db9f959f11f6608cfa10d4d00a2f20636d
Mikki99 <milan007miletic@gmail.com>
2024-05-20 00:36:12 +0200

    running for 2 prompts

--- eval/eval_all_verbal_analogies.sh
+++ eval/eval_all_verbal_analogies.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=EvalVerbalAnalogies_mistral7b
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=04:00:00
+#SBATCH --output=eval_llama3_story_analogies_two_stage_%A.out
+
+module purge
+module load 2022
+module load Anaconda3/2022.05
+
+source activate abstract
+
 # Define the list of models
-# models=("llama3" "mistral7b" "starling7b-beta")  # Add your models to this list
 models=("mistral7b")  # Add your models to this list
 
-
 # Define the directory to search for text files
-directory="prompt_templates/verbal_analogy"  # Change this to your target directory
+directory="prompt_templates/verbal_analogy/"  # Change this to your target directory
 
 # Iterate over each model
 for model in "${models[@]}"; do 
@@ -17,6 +29,6 @@ for model in "${models[@]}"; do
     find "$directory" -type f -name '*.txt' | while read -r file; do
         # Run the command for each text file with the current model
         echo $file
-        python3 eval/eval_verbal_analogy_new.py --model "$model" --prompt "$file"
+        srun python -u eval/eval_story_analogy_new.py --model "$model" --prompt "$file"
     done
 done
