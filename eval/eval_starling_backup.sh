ed7aed2bde384936d169c172a188c3f840dc1822
Mikki99 <milan007miletic@gmail.com>
2024-05-21 10:47:35 +0200

    updated job file

--- eval/eval_starling_backup.sh
+++ eval/eval_starling_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu_vis
+#SBATCH --partition=gpu
 #SBATCH --gpus=1
-#SBATCH --job-name=EvalVerbalAnalogies_mistral7b
+#SBATCH --job-name=EvalStoryAnalogies_Starling7b
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
 #SBATCH --time=04:00:00
-#SBATCH --output=eval_verbal_analogies_basic_no_%A.out
+#SBATCH --output=eval_starling7b_story_analogies_two_stage_%A.out
 
 module purge
 module load 2022
@@ -15,10 +15,10 @@ module load Anaconda3/2022.05
 source activate abstract
 
 # Define the list of models
-models=("mistral7b")  # Add your models to this list
+models=("starling7b-beta")  # Add your models to this list
 
 # Define the directory to search for text files
-directory="prompt_templates/verbal_analogy/"  # Change this to your target directory
+directory="prompt_templates/story_analogies"  # Change this to your target directory
 
 # Iterate over each model
 for model in "${models[@]}"; do 
